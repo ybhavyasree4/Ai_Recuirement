8bb61b4ff384cdbfd0d25e4702882ee7fb804f65
ybhavyasree4 <bhavyasreey54@gmail.com>
2026-09-09 15:26:07 +0530

    Fix CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -62,7 +62,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "https://ai-recuirement.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
